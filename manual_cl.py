7ac219691d98cb40f21d1aa22675e0df465afc28
Salma Khalil <salmakhalil@Salmas-MacBook-Pro-2.local>
2019-08-07 11:38:41 +0200

    still buggy. adding plotting. fixing saver error

--- manual_cl.py
+++ manual_cl.py
@@ -11,6 +11,7 @@ import datetime
 from itertools import permutations
 import imageio
 import csv
+import shelve
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import SubprocVecEnv, DummyVecEnv, VecNormalize
 from stable_baselines import PPO2
@@ -21,8 +22,8 @@ import xml.etree.ElementTree as ET
 
 
 def alter_env(exp_type, variant):
+	print('LOGGER: alter_env_start')
 	xml_path = os.path.join(gym_real.__path__[0], "envs/assets/real.xml")
-	print(xml_path)
 
 	tree = ET.parse(xml_path)
 	root = tree.getroot()
@@ -37,8 +38,10 @@ def alter_env(exp_type, variant):
 			pos.set("pos", str(variant) + " 0 "  + str(abs(-0.1) + 0.7))
 
 	tree.write(xml_path)
+	print('LOGGER: alter_env_end')
 
-def load_checkpoint(model_name, checkpoint, run_path):
+def load_checkpoint(checkpoint, run_path):
+	print('LOGGER: load_checkpoint_start')
 	checkpoint_log = run_path + '/checkpoint'
 	checkpoint_keeper = []
 	with open(checkpoint_log) as csv_file:
@@ -48,33 +51,47 @@ def load_checkpoint(model_name, checkpoint, run_path):
 			if line_count != 0:
 				checkpoint_keeper.append(row[1])
 			line_count += 1
+	print('LOGGER: load_checkpoint_end')
 	return run_path + '/' + checkpoint_keeper[checkpoint] + '.pkl'
 
-def log_experiments(exp_num, exp_type, variants, model_names, exp_log):
-	log_dict = dict()
-	log_dict['exp' + str(exp_num)] = {'type': exp_type, 'variants': variants, 'models': model_names}
-	with open(exp_log, 'a') as csv_file:
-		csv_writer = csv.writer(csv_file)
-		csv_writer.writerows(log_dict.items())
+def log_experiments(exp_num, exp_type, variants, model_names, exp_log, log_dict):
+	print('LOGGER: log_experiments_start')
+	# file = shelve.open(exp_log)
+	# file['exp' + exp_num] = [exp_type, variants, model_names]
+	# file.close()
+	print('exp'  + exp_num)
+	# with open(exp_log, 'a') as csv_file:
+	# 	csv_writer = csv.writer(csv_file, delimiter=',')
+	# 	if os.stat(exp_log).st_size == 0:
+	# 		csv_writer.writerow(['exp', 'type', 'variants', 'models'])
+	# 	csv_writer.writerow(['exp' + str(exp_num), exp_type, variants, model_names])
+	print('LOGGER: log_experiments_end')
 
-def run_experiment(exp_num, exp_type, variants, n_cpu, step_total, exp_log):
+def run_experiment(exp_num, exp_type, variants, n_cpu, step_total, exp_log, log_dict):
+	print('LOGGER: run_experiments_start')
 	model_names = []
 	run_path = ''
 	for order, variant in enumerate(variants):
+		print('LOGGER: run_experiments_mid')
 		alter_env(exp_type, variant)
 		env = gym.make("Real-v0")
+		env = Monitor(env, 'tf_save', allow_early_resets=True)
 		env = SubprocVecEnv([lambda: env for i in range(n_cpu)])
+		print('LOGGER: run_experiments_mid2')
 		if order == 0:
 			model = PPO2(MlpPolicy, env, verbose=1, tensorboard_log="./tensorboard_log/")
 		else:
-			load_name = load_checkpoint(model_names[-1], -1, run_path)
+			load_name = load_checkpoint(-1, run_path)
 			model = PPO2.load(load_name, env=env)
+		print('LOGGER: run_experiments_mid3')
 		model_names.append(model.model_name)
 		run_path = model.graph_dir
 		model.learn(total_timesteps=step_total)
 		env.close()
 		del model, env
-	log_experiments(exp_num, exp_type, variants, model_names, exp_log)
+		print('LOGGER: run_experiments_mid4')
+	log_experiments(exp_num, exp_type, variants, model_names, exp_log, log_dict)
+	print('LOGGER: run_experiments_end')
 
 if __name__ == "__main__":
 	n_cpu = 8
@@ -84,13 +101,15 @@ if __name__ == "__main__":
 	leg_lengths = [i * -0.1 for i in range(1, 5)]
 	goal_diss = [i * -2 for i in range(2, 6)]
 
-	exp_log = 'experiment_logs.csv'
+	exp_log = 'experiment_logs'
+	log_dict = {}
 
 	leg_type = 'LEG_LENGTH'
 	dis_type = 'GOAL_DIS'
 	exp_types = [dis_type, leg_type]
-	for _ in range(5):
-		for exp_type in exp_types:
+	for i in range(5):
+		print('LOGGER: main_loop_start')
+		for j, exp_type in enumerate(exp_types):
 			if exp_type == leg_type:
 				variant = leg_lengths
 			elif exp_type == dis_type:
@@ -98,7 +117,8 @@ if __name__ == "__main__":
 			perm_2 = list(permutations(variant, 2))
 			perm_4 = list(permutations(variant))
 			perms = perm_2 + perm_4
-			for i, perm in enumerate(perms):
-				print('PERM', perms)
-				run_experiment(i, exp_type, perm, n_cpu, step_total, exp_log)
+			print('LOGGER: main_loop_mid')
+			for k, perm in enumerate(perms):
+				print('LOGGER: main_loop_midstart')
+				run_experiment(str(i) + '_' + str(j) + '_' + str(k), exp_type, perm, n_cpu, step_total, exp_log, log_dict)
 
--- plotter.py
+++ plotter.py
@@ -1,6 +1,7 @@
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+import shelve
 
 def moving_average(values, window):
     """
@@ -12,44 +13,58 @@ def moving_average(values, window):
     weights = np.repeat(1.0, window) / window
     return np.convolve(values, weights, 'valid')
 
-run_path = 'tf_save/12:55:19-06-08-19/'
-checkpoint_log = run_path + 'checkpoint'
-checkpoint_keeper = []
-with open(checkpoint_log) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=' ')
-    line_count = 0
-    for row in csv_reader:
-    	if line_count != 0:
-    		checkpoint_keeper.append(row[1])
-    	line_count += 1
+def load_checkpoint(checkpoint, run_path):
+    checkpoint_log = run_path + '/checkpoint'
+    checkpoint_keeper = []
+    with open(checkpoint_log) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=' ')
+        line_count = 0
+        for row in csv_reader:
+            if line_count != 0:
+                checkpoint_keeper.append(row[1])
+            line_count += 1
+    return run_path + '/' + checkpoint_keeper[checkpoint] + '.pkl'
 
-plot_log = run_path + checkpoint_keeper[200] + '.csv'
-print(len(checkpoint_keeper))
-reward, length, time = [], [], []
-with open(plot_log) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-    	if line_count != 0:
-    		reward.append(float(row[0]))
-    		length.append(int(row[1]))
-    		time.append(float(row[2]))
-    	line_count += 1
+exp_logger = 'experiment_logs'
+# log_dict = {}
+file = shelve.open(exp_logger)
+keys = list(file.keys())
+keys.sort()
+for i in keys:
+    print(i, file[i])
+file.close()
+# with open(exp_logger) as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=',')
+#     for row in csv_reader:
+#         print(ast.literal_eval(row[3]))
 
-length = np.cumsum(length)
-print(len(length))
+# plot_log = run_path + checkpoint_keeper[-1] + '.csv'
+# print(len(checkpoint_keeper))
+# reward, length, time = [], [], []
+# with open(plot_log) as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=',')
+#     line_count = 0
+#     for row in csv_reader:
+#     	if line_count != 0:
+#     		reward.append(float(row[0]))
+#     		length.append(int(row[1]))
+#     		time.append(float(row[2]))
+#     	line_count += 1
 
-# reward = moving_average(reward, window=1)
-# Truncate length
-length = length[len(length) - len(reward):]
+# length = np.cumsum(length)
+# print(len(length))
 
-title = 'my_plot'
-fig = plt.figure(title)
-plt.plot(length, reward)
-plt.xlabel('Number of Timesteps')
-plt.ylabel('Rewards')
-# plt.title(title + " Smoothed")
-# plt.savefig(save_name + ".png")
-# plt.savefig(save_name + ".eps")
-# print("plots saved...")
-plt.show()
+# # reward = moving_average(reward, window=1)
+# # Truncate length
+# length = length[len(length) - len(reward):]
+
+# title = 'my_plot'
+# fig = plt.figure(title)
+# plt.plot(length, reward)
+# plt.xlabel('Number of Timesteps')
+# plt.ylabel('Rewards')
+# # plt.title(title + " Smoothed")
+# # plt.savefig(save_name + ".png")
+# # plt.savefig(save_name + ".eps")
+# # print("plots saved...")
+# plt.show()

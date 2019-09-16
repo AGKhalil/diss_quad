6857063451fe8c921fcb1d136d37cd5f2b5e55fe
AGKhalil <elghazalykhalil@gmail.com>
2019-09-16 15:35:39 +0100

    Docs added to files

--- manual_cl.py
+++ manual_cl.py
@@ -1,3 +1,5 @@
+"""Summary
+"""
 import os
 import time
 import subprocess
@@ -24,108 +26,148 @@ import xml.etree.ElementTree as ET
 
 
 def alter_env(exp_type, variant):
-	xml_path = os.path.join(gym_real.__path__[0], "envs/assets/real.xml")
+    """Alters leg length, or distance from goal, of the gym-real quadrupedal environment.
+    
+    Args:
+        exp_type (str): leg length or distnace from goal
+        variant (float): variable to change
+    """
+    xml_path = os.path.join(gym_real.__path__[0], "envs/assets/real.xml")
 
-	tree = ET.parse(xml_path)
-	root = tree.getroot()
-	if exp_type == 'LEG_LENGTH':
-		for geom in root.findall("worldbody/body/body/body/body/geom"):
-			geom.set("fromto", "0 0 0 0 0 " + str(variant))
+    tree = ET.parse(xml_path)
+    root = tree.getroot()
+    if exp_type == 'LEG_LENGTH':
+        for geom in root.findall("worldbody/body/body/body/body/geom"):
+            geom.set("fromto", "0 0 0 0 0 " + str(variant))
 
-		for pos in root.findall("worldbody/body/[@name='torso']"):
-			pos.set("pos", "-10.0 0 " + str(abs(variant) + 0.7))
-	elif exp_type == 'GOAL_DIS':
-		for pos in root.findall("worldbody/body/[@name='torso']"):
-			pos.set("pos", str(variant) + " 0 "  + str(abs(-0.1) + 0.7))
+        for pos in root.findall("worldbody/body/[@name='torso']"):
+            pos.set("pos", "-10.0 0 " + str(abs(variant) + 0.7))
+    elif exp_type == 'GOAL_DIS':
+        for pos in root.findall("worldbody/body/[@name='torso']"):
+            pos.set("pos", str(variant) + " 0 " + str(abs(-0.1) + 0.7))
 
-	tree.write(xml_path)
+    tree.write(xml_path)
+
 
 def load_checkpoint(checkpoint, run_path):
-	checkpoint_log = run_path + '/checkpoint'
-	checkpoint_keeper = []
-	with open(checkpoint_log) as csv_file:
-		csv_reader = csv.reader(csv_file)
-		for row in csv_reader:
-			checkpoint_keeper.append(row[0])
-	return run_path + '/' + checkpoint_keeper[checkpoint] + '.pkl'
+    """Loads a model checkpoint pkl file.
+    
+    Args:
+        checkpoint (int): checkpoint number
+        run_path (str): model path
+    
+    Returns:
+        str: path for loading the appropriate model and at at certain checkpoint
+    """
+    checkpoint_log = run_path + '/checkpoint'
+    checkpoint_keeper = []
+    with open(checkpoint_log) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            checkpoint_keeper.append(row[0])
+    return run_path + '/' + checkpoint_keeper[checkpoint] + '.pkl'
+
 
 def log_experiments(exp_num, exp_type, variants, model_names, exp_log, log_dict):
-	log_dict[exp_type, variants].append(model_names)
-	with open(exp_log, "wb") as file_:
-		cloudpickle.dump(log_dict, file_)
-	# file = shelve.open(exp_log)
-	# file['exp' + exp_num] = [exp_type, variants, model_names]
-	# file.close()
-	print('exp'  + exp_num)
-	return log_dict
-	# with open(exp_log, 'a') as csv_file:
-	# 	csv_writer = csv.writer(csv_file, delimiter=',')
-	# 	if os.stat(exp_log).st_size == 0:
-	# 		csv_writer.writerow(['exp', 'type', 'variants', 'models'])
-	# 	csv_writer.writerow(['exp' + str(exp_num), exp_type, variants, model_names])
+    """Records the models run for their corresponding curricula. This allows for easy lookup for results analysis.
+    
+    Args:
+        exp_num (int): experiemnt number
+        exp_type (str): type of experiemnt. leg length or goal distance
+        variants (int): curriculum list of ints
+        model_names (str): list of model names for each part of the curriculum
+        exp_log (str): log file path
+        log_dict (Dict): dictionary saving logging data
+    
+    Returns:
+        Dict: dictionary saving logging data
+    """
+    log_dict[exp_type, variants].append(model_names)
+    with open(exp_log, "wb") as file_:
+        cloudpickle.dump(log_dict, file_)
+    print('exp' + exp_num)
+    return log_dict
+
 
 def run_experiment(exp_num, exp_type, variants, n_cpu, step_total, exp_log, log_dict):
-	model_names = []
-	run_path = ''
-	for order, variant in enumerate(variants):
-		alter_env(exp_type, variant)
-		env = gym.make("Real-v0")
-		env = Monitor(env, 'tf_save', allow_early_resets=True)
-		env = SubprocVecEnv([lambda: env for i in range(n_cpu)])
-		if order == 0:
-			model = PPO2(MlpPolicy, env, verbose=0, tensorboard_log="./tensorboard_log/")
-		else:
-			load_name = load_checkpoint(-1, run_path)
-			model = PPO2.load(load_name, env=env)
-		model_names.append(model.model_name)
-		run_path = model.graph_dir
-		model.learn(total_timesteps=step_total)
-		env.close()
-		del model, env
-	log_dict = log_experiments(exp_num, exp_type, variants, model_names, exp_log, log_dict)
-	return log_dict
+    """Runs a curriculum experiment while logging everything.
+    
+    Args:
+        exp_num (int): experiemnt number
+        exp_type (str): type of experiemnt. leg length or goal distance
+        variants (int): curriculum list of ints
+        n_cpu (int): number of CPUs used for training
+        step_total (int): total number of training steps
+        exp_log (str): log file path
+        log_dict (Dict): dictionary saving logging data
+    
+    Returns:
+        Dict: dictionary saving logging data
+    """
+    model_names = []
+    run_path = ''
+    for order, variant in enumerate(variants):
+        alter_env(exp_type, variant)
+        env = gym.make("Real-v0")
+        env = Monitor(env, 'tf_save', allow_early_resets=True)
+        env = SubprocVecEnv([lambda: env for i in range(n_cpu)])
+        if order == 0:
+            model = PPO2(MlpPolicy, env, verbose=0,
+                         tensorboard_log="./tensorboard_log/")
+        else:
+            load_name = load_checkpoint(-1, run_path)
+            model = PPO2.load(load_name, env=env)
+        model_names.append(model.model_name)
+        run_path = model.graph_dir
+        model.learn(total_timesteps=step_total)
+        env.close()
+        del model, env
+    log_dict = log_experiments(
+        exp_num, exp_type, variants, model_names, exp_log, log_dict)
+    return log_dict
+
 
 if __name__ == "__main__":
-	n_cpu = 20
-	n_step = 128
-	desired_log_pts = 1000
-	step_total = desired_log_pts * n_cpu * n_step
-	leg_lengths_1 = [i * -0.1 for i in range(1, 5)]
-	leg_lengths_2 = [i * -0.1 for i in range(1, 9, 2)]
-	goal_diss = [i * -2 for i in range(2, 6)]
+    n_cpu = 20
+    n_step = 128
+    desired_log_pts = 1000
+    step_total = desired_log_pts * n_cpu * n_step
+    leg_lengths_1 = [i * -0.1 for i in range(1, 5)]
+    leg_lengths_2 = [i * -0.1 for i in range(1, 9, 2)]
+    goal_diss = [i * -2 for i in range(2, 6)]
 
-	log_dir = '/media/brl/Seagate Expansion Drive1/khalil/tf_save/exp_log/'
-	os.makedirs(log_dir, exist_ok=True)
-	log_dict = defaultdict(list)
+    log_dir = '/media/brl/Seagate Expansion Drive1/khalil/tf_save/exp_log/'
+    os.makedirs(log_dir, exist_ok=True)
+    log_dict = defaultdict(list)
 
-	leg_type = 'LEG_LENGTH'
-	dis_type = 'GOAL_DIS'
-	exp_types = [leg_type]
-	lengths = 0
-	for exp in [0, 1]:
-		for l in range(2, 3):
-			for i in range(5):
-				for j, exp_type in enumerate(exp_types):
-					if exp_type == leg_type:
-						if exp == 0:
-							variant = leg_lengths_1
-							baseline = [(m,) * l for m in variant]
-						else:
-							variant = leg_lengths_2
-							baseline = [(m,) * l for m in variant[2:]]
-					elif exp_type == dis_type:
-						variant = goal_diss
-					perm_vars = list(permutations(variant, l))
-					perms = baseline + perm_vars
-					for k, perm in enumerate(perms):
-						print(perm)
-						exp_name = '{:%d%m%y_%H:%M:%S}'.format(datetime.datetime.now())
-						exp_log = log_dir + exp_name
-						log_dict = run_experiment(str(i) + '_' + str(j) + '_' + str(k), exp_type, perm, n_cpu, step_total, exp_log, log_dict)
-						checkpoint_log = log_dir + 'checkpoint'
-						print(checkpoint_log)
-						with open(checkpoint_log, mode='a') as employee_file:
-							employee_writer = csv.writer(employee_file)
-							employee_writer.writerow([exp_name])
-
-
+    leg_type = 'LEG_LENGTH'
+    dis_type = 'GOAL_DIS'
+    exp_types = [leg_type]
+    lengths = 0
+    for exp in [0, 1]:
+        for l in range(2, 3):
+            for i in range(5):
+                for j, exp_type in enumerate(exp_types):
+                    if exp_type == leg_type:
+                        if exp == 0:
+                            variant = leg_lengths_1
+                            baseline = [(m,) * l for m in variant]
+                        else:
+                            variant = leg_lengths_2
+                            baseline = [(m,) * l for m in variant[2:]]
+                    elif exp_type == dis_type:
+                        variant = goal_diss
+                    perm_vars = list(permutations(variant, l))
+                    perms = baseline + perm_vars
+                    for k, perm in enumerate(perms):
+                        print(perm)
+                        exp_name = '{:%d%m%y_%H:%M:%S}'.format(
+                            datetime.datetime.now())
+                        exp_log = log_dir + exp_name
+                        log_dict = run_experiment(str(
+                            i) + '_' + str(j) + '_' + str(k), exp_type, perm, n_cpu, step_total, exp_log, log_dict)
+                        checkpoint_log = log_dir + 'checkpoint'
+                        print(checkpoint_log)
+                        with open(checkpoint_log, mode='a') as employee_file:
+                            employee_writer = csv.writer(employee_file)
+                            employee_writer.writerow([exp_name])
--- plotter.py
+++ plotter.py
@@ -1,10 +1,29 @@
+"""Script written to generate results from the training log files. The results generated are plots and gifs.
+
+Attributes:
+    baselines_agg (bool): flag to aggregate results for baseline plots
+    counter (int): used for plotting baselines of list A and B seprately
+    exp_file (str): logger file path
+    exp_logger (str): logger directory path
+    leg_lengths_1 (int): list A curriculum leg lengths
+    leg_lengths_2 (int): list B curriculum leg lengths
+    names (list): model names
+    plot_agg_dir (str): plots aggregate directory
+    plot_dir (str): plots directory
+"""
 import os
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+from itertools import permutations
 import shelve
 from collections import defaultdict
 import cloudpickle
+import gym
+import gym_real
+from stable_baselines import PPO2
+from stable_baselines.common.vec_env import DummyVecEnv
+import imageio
 
 def moving_average(values, window):
     """
@@ -12,11 +31,28 @@ def moving_average(values, window):
     :param values: (numpy array)
     :param window: (int)
     :return: (numpy array)
+    
+    Args:
+        values (int): list of values to do moving average for
+        window (int): window of moving average
+    
+    Returns:
+        int: new list of moving average applied to the input
     """
     weights = np.repeat(1.0, window) / window
     return np.convolve(values, weights, 'valid')
 
-def load_checkpoint(run_path, checkpoint=-1):
+def load_checkpoint(run_path, checkpoint=-1, csv_ind=True):
+    """Loads a model's last checkpoint csv or pkl file
+    
+    Args:
+        run_path (str): model path
+        checkpoint (str, last checpoint by default): checkpoint to load
+        csv_ind (bool, loads csv by default): Description
+    
+    Returns:
+        str: path to correct file
+    """
     checkpoint_log = run_path + '/checkpoint'
     checkpoint_keeper = []
     with open(checkpoint_log) as csv_file:
@@ -26,10 +62,22 @@ def load_checkpoint(run_path, checkpoint=-1):
             if line_count != 0:
                 checkpoint_keeper.append(row[0])
             line_count += 1
-    return run_path + '/' + checkpoint_keeper[checkpoint] + '.csv'
+    if csv_ind:
+        return run_path + '/' + checkpoint_keeper[checkpoint] + '.csv'
+    else:
+        return run_path + '/' + checkpoint_keeper[checkpoint] + '.pkl'
 
 def load_logger_chkpt(run_path, checkpoint=-1):
-    checkpoint_log = run_path + '/checkpoint'
+    """Loads most recent experiment log file
+    
+    Args:
+        run_path (str): file path
+        checkpoint (int, last checkpoint by default): checkpoint number
+    
+    Returns:
+        str: path to correct log file at the needed checkpoint
+    """
+    checkpoint_log = run_path + '/checkpoint_1'
     checkpoint_keeper = []
     with open(checkpoint_log) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=' ')
@@ -38,6 +86,14 @@ def load_logger_chkpt(run_path, checkpoint=-1):
     return run_path + checkpoint_keeper[-1]
 
 def get_plot_csv(plot_log):
+    """Extract results from csv file
+    
+    Args:
+        plot_log (str): csv file path
+    
+    Returns:
+        int, int: length, reward
+    """
     reward, length, time = [], [], []
     with open(plot_log) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
@@ -53,6 +109,15 @@ def get_plot_csv(plot_log):
     return length, reward
 
 def plot_graph(plot_name, save_name, length, reward, divider):
+    """Plots a graph of reward vs time
+    
+    Args:
+        plot_name (str): name of plot
+        save_name (str): save name of plot
+        length (int): list of time steps
+        reward (int): list of rewards
+        divider (int): timestep of task switch
+    """
     reward = moving_average(reward, window=30)
     # Truncate length
     length = length[len(length) - len(reward):]
@@ -67,46 +132,121 @@ def plot_graph(plot_name, save_name, length, reward, divider):
     plt.ylim([0, 800])
     plt.savefig(save_name + ".png")
     plt.savefig(save_name + ".eps")
-    # plt.show()
     plt.close()
 
 def aggregate_results(length, agg_reward, divider):
+    """Aggregates the results of seeds for an experiment
+    
+    Args:
+        length (int): time steps of results
+        agg_reward (int): list of rewards per seed
+        divider (int): list of timesteps at task swtich
+    
+    Returns:
+        int: aggregate data needed to plot results
+    """
     max_r, min_r, avg_r = [], [], []
-    # print(len(agg_reward))
-    # print([len(i) for i in agg_reward])
     data_length = min([len(i) for i in agg_reward])
     agg_reward = [i[:data_length] for i in agg_reward]
-    # print([len(i) for i in agg_reward])
-    # print(data_length)
     np.asarray(agg_reward)
     max_r = moving_average(np.max(agg_reward, 0), window=30)
     min_r = moving_average(np.min(agg_reward, 0), window=30)
     avg_r = moving_average(np.mean(agg_reward, axis=0), window=30)
+    std = moving_average(np.std(agg_reward, axis=0), window=30)
         
     # Truncate length
     length = length[len(length) - len(avg_r):]
-    return max_r, min_r, avg_r, length, divider
+    return max_r, min_r, avg_r, std, length, divider
 
 def plot_aggregate(plot_name, save_name, lengths, agg_rewards, dividers, labels):
-    title = plot_name
-    colors = ['#7ebcff', '#ffcccb']
+    """Plots the aggregate cumulative reward vs time of all seeds of an experiment.
+    
+    Args:
+        plot_name (str): name of plot
+        save_name (str): save name of plot
+        lengths (int): list of time steps per seed
+        agg_rewards (int): list of rewards per seed
+        dividers (int): list of dividers per seed
+        labels (str): list of model names
+    """
+    title = plot_name[0] + plot_name[1]
+    widths = [i * 0.5 for i in range(2, 5)] + [i * 0.2 for i in range(5, 9)]
+    alphas = [i * 0.1 for i in range(8)]
+    colors = ['#db4437', '#4285f4', '#f4b400', '#0f9d58', '#00796b', '#5c6bc0', '#00acc1', '#ff7043']
     fig = plt.figure(title)
     for i in range(len(lengths)):
-        max_r, min_r, avg_r, length, divider = aggregate_results(lengths[i], agg_rewards[i], dividers[i])
-        for j in divider:
-            plt.axvline(x=j, linestyle='--', color='#ccc5c6')
-        plt.plot(length, avg_r, label=labels[i])
-        plt.fill_between(length, min_r, max_r, color=colors[i], alpha=0.4)
-    plt.xlabel('Number of Timesteps')
+        max_r, min_r, avg_r, std, length, divider = aggregate_results(lengths[i], agg_rewards[i], dividers[i])
+        plt.plot(length, avg_r, label=labels[i], color=colors[i], linewidth=widths[i])
+        plt.fill_between(length, min_r, max_r, color=colors[i], alpha=0.1)
+    for i, j in enumerate(divider):
+        if i == 0:
+            plt.axvline(x=j, linestyle=':', color='#000000', label='end of training instance')
+        else:
+            plt.axvline(x=j, linestyle=':', color='#000000')
+    plt.xlabel('Timesteps')
     plt.legend()
-    plt.ylabel('Rewards')
+    plt.ylabel('Cumulative Reward')
     plt.title(title)
     plt.ylim([0, 800])
     plt.xlim([0, 5500000])
     plt.savefig(save_name + ".png")
-    plt.savefig(save_name + ".eps")
-    # plt.show()
+    plt.savefig(save_name + ".pdf")
     plt.close()
+    std_fig = plt.figure()
+    for i in range(len(lengths)):
+        max_r, min_r, avg_r, std, length, divider = aggregate_results(lengths[i], agg_rewards[i], dividers[i])
+        plt.plot(length, std, label=labels[i], color=colors[i], linewidth=widths[i])
+    for i, j in enumerate(divider):
+        if i == 0:
+            plt.axvline(x=j, linestyle=':', color='#000000', label='end of training instance')
+        else:
+            plt.axvline(x=j, linestyle=':', color='#000000')
+    plt.xlabel('Timesteps')
+    plt.legend()
+    plt.ylabel('Running Standard Deviation')
+    plt.title(plot_name[0] + ' Standard Deviation')
+    plt.ylim([0, 300])
+    plt.xlim([0, 5500000])
+    plt.savefig(save_name + "std.png")
+    plt.savefig(save_name + "std.pdf")
+    plt.close()
+
+def make_me_a_gif(load_name, model_name):
+    """Creates a gif of the agent.
+    
+    Args:
+        load_name (str): load pkl name
+        model_name (str): model name
+    """
+    loader = load_checkpoint('/media/brl/Seagate Expansion Drive1/khalil/tf_save/' + load_name, checkpoint=-1, csv_ind=False)
+    gif_dir = '/media/brl/Seagate Expansion Drive1/khalil/report_gifs/'
+    os.makedirs(gif_dir, exist_ok=True)
+    save_str = gif_dir + model_name + '.gif'
+    images = []
+    env = gym.make('Real-v0')
+    model = PPO2.load(loader)
+    obs = env.reset()
+    done = False
+    step, reward = 0, 0
+    img = env.sim.render(
+        width=300, height=300)
+    for _ in range(1000):
+        action, _ = model.predict(obs)
+        obs, reward, done, _ = env.step(action)
+        img = env.sim.render(
+            width=300, height=300, camera_name='isometric_view') #camera_name="isometric_view"
+        images.append(np.flipud(img))
+        step += 1
+
+    env.close()
+    del env
+    del model
+
+    print("creating gif...")
+    print("saving gif at:", save_str)
+    imageio.mimsave(save_str, [np.array(img)
+                               for i, img in enumerate(images) if i % 2 == 0], fps=29)
+    print("gif created...")
 
 plot_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "/media/brl/Seagate Expansion Drive1/khalil/plot_saves/")
 plot_agg_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "/media/brl/Seagate Expansion Drive1/khalil/plot_saves/agg/")
@@ -117,43 +257,31 @@ exp_logger = '/media/brl/Seagate Expansion Drive1/khalil/tf_save/exp_log/'
 exp_file = load_logger_chkpt(exp_logger)
 with open(exp_file, "rb") as file:
     data = cloudpickle.load(file)
-# print(data['LEG_LENGTH', (-0.1, -0.1)])
 
-# file = shelve.open(exp_logger)
-# keys = list(file.keys())
-# keys.sort()
-# data = defaultdict(list)
-# for exp in keys:
-#     data[file[exp][0], file[exp][1]].append(file[exp][2])
-#     model_names = file[exp][2]
-#     # print(exp, file[exp][0], file[exp][1], file[exp][2])
-#     length, reward = [], []
-#     cum_l = 0
-#     cum_l_list = []
-#     plot_name = exp + file[exp][0] + str(file[exp][1])
-#     save_name = plot_dir + file[exp][0] + str(file[exp][1]) + str(file[exp][2])
-#     for model_name in model_names:
-#         run_path = '/media/brl/Seagate Expansion Drive1/khalil/tf_save/' + model_name
-#         l, r = get_plot_csv(load_checkpoint(run_path))
-#         length.extend(l + cum_l)
-#         reward.extend(r)
-#         # print(model_name, len(r), len(l), l[-1])
-#         cum_l += l[-1]
-#         cum_l_list.append(cum_l)
-#     plot_graph(plot_name, save_name, length, reward, cum_l_list)
-# file.close()
-# print(data['LEG_LENGTH', (-0.1, -0.1)])
-
+baselines_agg = True
+leg_lengths_1 = [i * -0.1 for i in range(1, 5)]
+leg_lengths_2 = [i * -0.1 for i in range(1, 9, 2)]
+counter = 0
 for key, val in data.items():
     lengths, agg_rewards, cum_l_lists = [], [], []
-    plot_name = key[0] + '(' + '%.1f' % abs(key[1][0]) + ',' + '%.1f' % abs(key[1][-1]) + ')'
-    save_name = plot_agg_dir + plot_name + 'agg'
     if key[1][0] != key[1][-1]:
+        plot_name = ['Leg lengths: (' + '%.1f' % abs(key[1][0]) + ',' + '%.1f' % abs(key[1][-1]) + ') ', 'Learning Curves']
+        save_name = plot_agg_dir + '(' +  '%.1f' % abs(key[1][0]) + ',' + '%.1f' % abs(key[1][-1]) + ')' + 'agg'
         values = [val[:5], data[key[0], (key[1][-1], key[1][-1])][:5]]
-        labels = ['(' + '%.1f' % abs(key[1][0]) + ',' + '%.1f' % abs(key[1][-1]) + ') curriculum', '(' + '%.1f' % abs(key[1][-1]) + ',' + '%.1f' % abs(key[1][-1]) + ') baseline']
-    else:
-        values = [val[:5]]
-        labels = ['(' + '%.1f' % abs(key[1][0]) + ',' + '%.1f' % abs(key[1][-1]) + ') baseline']
+        labels = ['(' + '%.1f' % abs(key[1][0]) + ',' + '%.1f' % abs(key[1][-1]) + ') curriculum', '(' + '%.1f' % abs(key[1][-1]) + ',' + '%.1f' % abs(key[1][-1]) + ') tabula rasa']
+    elif key[1][0] == key[1][-1] and baselines_agg:
+        if counter == 0:
+            plot_name = ['List A Baseline ', 'Learning Curves']
+            save_name = plot_agg_dir + 'baselines_aggA'
+            base_length = leg_lengths_1
+            counter += 1
+        else:
+            plot_name = ['List B Baseline', 'Learning Curves']
+            save_name = plot_agg_dir + 'baselines_aggB'
+            base_length = leg_lengths_2
+            baselines_agg = False
+        values = [data[key[0], (i, i)] for i in base_length]
+        labels = ['(' + '%.1f' % abs(i) + ',' + '%.1f' % abs(i) + ')' for i in base_length]
     for i in values:
         agg_reward = []
         for model_names in i:
@@ -171,4 +299,92 @@ for key, val in data.items():
         agg_rewards.append(agg_reward)
         lengths.append(length)
         cum_l_lists.append(cum_l_list)
-    plot_aggregate(plot_name + 'agg' + str(len(val)), save_name, lengths, agg_rewards, cum_l_lists, labels)
+    plot_aggregate(plot_name, save_name, lengths, agg_rewards, cum_l_lists, labels)
+
+names = ['listA', 'listB']
+for v, variant in enumerate([leg_lengths_1, leg_lengths_2]):
+    perm_vars = list(permutations(variant, 2))
+    for var in variant:
+        lengths, agg_rewards, cum_l_lists = [], [], []
+        kys = [j for j in perm_vars if var == j[-1]] + [(var, var)]
+        values = [data['LEG_LENGTH', ky][:5] for ky in kys]
+        plot_name = ['Final leg length: (' + '%.1f' % abs(var) + ') ', 'Learning Curves']
+        save_name = plot_agg_dir + names[v] + '(' + '%.1f' % abs(var) + ')' + 'agg'
+        labels = ['(' + '%.1f' % abs(ky[0]) + ',' + '%.1f' % abs(ky[-1]) + ') curriculum' for ky in kys[:-1]] + ['(' + '%.1f' % abs(ky[0]) + ',' + '%.1f' % abs(ky[-1]) + ') tabula rasa' for ky in kys[-1:]]
+        for i in values:
+            agg_reward = []
+            for model_names in i:
+                length, reward = [], []
+                cum_l = 0
+                cum_l_list = []
+                for model_name in model_names:
+                    run_path = '/media/brl/Seagate Expansion Drive1/khalil/tf_save/' + model_name
+                    l, r = get_plot_csv(load_checkpoint(run_path))
+                    length.extend(l + cum_l)
+                    reward.extend(r)
+                    cum_l += l[-1]
+                    cum_l_list.append(cum_l)
+                agg_reward.append(reward)
+            agg_rewards.append(agg_reward)
+            lengths.append(length)
+            cum_l_lists.append(cum_l_list)
+        plot_aggregate(plot_name, save_name, lengths, agg_rewards, cum_l_lists, labels)
+
+for v, variant in enumerate([leg_lengths_1, leg_lengths_2]):
+    perm_vars = list(permutations(variant, 2))
+    for var in variant:
+        lengths, agg_rewards, cum_l_lists = [], [], []
+        kys = [j for j in perm_vars if var == j[0]] + [(var, var)]
+        values = [data['LEG_LENGTH', ky][:5] for ky in kys]
+        plot_name = ['Initial leg length: (' + '%.1f' % abs(var) + ') ', 'Learning Curves']
+        save_name = plot_agg_dir + names[v] + 'Initial(' + '%.1f' % abs(var) + ')' + 'agg'
+        labels = ['(' + '%.1f' % abs(ky[0]) + ',' + '%.1f' % abs(ky[-1]) + ') curriculum' for ky in kys[:-1]] + ['(' + '%.1f' % abs(ky[0]) + ',' + '%.1f' % abs(ky[-1]) + ') tabula rasa' for ky in kys[-1:]]
+        for i in values:
+            agg_reward = []
+            for model_names in i:
+                length, reward = [], []
+                cum_l = 0
+                cum_l_list = []
+                for model_name in model_names:
+                    run_path = '/media/brl/Seagate Expansion Drive1/khalil/tf_save/' + model_name
+                    l, r = get_plot_csv(load_checkpoint(run_path))
+                    length.extend(l + cum_l)
+                    reward.extend(r)
+                    cum_l += l[-1]
+                    cum_l_list.append(cum_l)
+                agg_reward.append(reward)
+            agg_rewards.append(agg_reward)
+            lengths.append(length)
+            cum_l_lists.append(cum_l_list)
+        plot_aggregate(plot_name, save_name, lengths, agg_rewards, cum_l_lists, labels)
+
+for key, val in data.items():
+    lengths, agg_rewards, cum_l_lists = [], [], []
+    if key[1][0] != key[1][-1]:
+        plot_name = ['Leg lengths: (' + '%.1f' % abs(key[1][0]) + ',' + '%.1f' % abs(key[1][-1]) + ') and (' + '%.1f' % abs(key[1][-1]) + ',' + '%.1f' % abs(key[1][0]) + ')', ' Learning Curves']
+        save_name = plot_agg_dir + '(' +  '%.1f' % abs(key[1][0]) + ',' + '%.1f' % abs(key[1][-1]) + ')' + 'viceversa'
+        values = [val[:5], data[key[0], (key[1][-1], key[1][0])][:5]]
+        labels = ['(' + '%.1f' % abs(key[1][0]) + ',' + '%.1f' % abs(key[1][-1]) + ') curriculum', '(' + '%.1f' % abs(key[1][-1]) + ',' + '%.1f' % abs(key[1][0]) + ') curriculum']
+    for i in values:
+        agg_reward = []
+        for model_names in i:
+            length, reward = [], []
+            cum_l = 0
+            cum_l_list = []
+            for model_name in model_names:
+                run_path = '/media/brl/Seagate Expansion Drive1/khalil/tf_save/' + model_name
+                l, r = get_plot_csv(load_checkpoint(run_path))
+                length.extend(l + cum_l)
+                reward.extend(r)
+                cum_l += l[-1]
+                cum_l_list.append(cum_l)
+            agg_reward.append(reward)
+        agg_rewards.append(agg_reward)
+        lengths.append(length)
+        cum_l_lists.append(cum_l_list)
+    plot_aggregate(plot_name, save_name, lengths, agg_rewards, cum_l_lists, labels)
+
+for key, val in data.items():
+    for v in val:
+        gif_name = '(' +  '%.1f' % abs(key[1][0]) + ',' + '%.1f' % abs(key[1][-1]) + ')' + v[-1]
+        make_me_a_gif(v[-1], gif_name)
